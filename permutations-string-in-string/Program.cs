5407a5e48c7b003fbd011bb04e4b16b9e6433964
toppggg <fpg662@alumni.ku.dk>
2021-12-13 22:29:44 +0100

    folder changed

---
+++ permutations-string-in-string/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Egen_Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
